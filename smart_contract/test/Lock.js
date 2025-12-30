c054b5db93ed350cfd4a7a069ceb0ba122a53a3d
Ayush765-spec <ayushsouthpoint@gmail.com>
2025-12-30 16:58:21 +0530

    polished Welcome page and started with the smart contract part

---
+++ smart_contract/test/Lock.js
@@ -0,0 +1,106 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Lock", function () {
+  async function deployOneYearLockFixture() {
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const ONE_GWEI = ethers.utils.parseEther("0.001");
+
+    const lockedAmount = ONE_GWEI;
+    const latestBlock = await ethers.provider.getBlock("latest");
+    const unlockTime = latestBlock.timestamp + ONE_YEAR_IN_SECS;
+
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Lock = await ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    await lock.deployed();
+
+    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right unlockTime", async function () {
+      const { lock, unlockTime } = await deployOneYearLockFixture();
+      expect(await lock.unlockTime()).to.equal(unlockTime);
+    });
+
+    it("Should set the right owner", async function () {
+      const { lock, owner } = await deployOneYearLockFixture();
+      expect(await lock.owner()).to.equal(owner.address);
+    });
+
+    it("Should receive and store the funds to lock", async function () {
+      const { lock, lockedAmount } = await deployOneYearLockFixture();
+      expect(await ethers.provider.getBalance(lock.address)).to.equal(lockedAmount);
+    });
+
+    it("Should fail if the unlockTime is not in the future", async function () {
+      const latestBlock = await ethers.provider.getBlock("latest");
+      const latestTime = latestBlock.timestamp;
+      const Lock = await ethers.getContractFactory("Lock");
+      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
+        "Unlock time should be in the future"
+      );
+    });
+  });
+
+  describe("Withdrawals", function () {
+    describe("Validations", function () {
+      it("Should revert with the right error if called too soon", async function () {
+        const { lock } = await deployOneYearLockFixture();
+        await expect(lock.withdraw()).to.be.revertedWith(
+          "You can't withdraw yet"
+        );
+      });
+
+      it("Should revert with the right error if called from another account", async function () {
+        const { lock, unlockTime, otherAccount } = await deployOneYearLockFixture();
+        
+        await ethers.provider.send("evm_setNextBlockTimestamp", [unlockTime]);
+        await ethers.provider.send("evm_mine", []);
+
+        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+          "You aren't the owner"
+        );
+      });
+
+      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+        const { lock, unlockTime } = await deployOneYearLockFixture();
+        
+        await ethers.provider.send("evm_setNextBlockTimestamp", [unlockTime]);
+        await ethers.provider.send("evm_mine", []);
+
+        await expect(lock.withdraw()).not.to.be.reverted;
+      });
+    });
+
+    describe("Events", function () {
+      it("Should emit an event on withdrawal", async function () {
+        const { lock, unlockTime, lockedAmount } = await deployOneYearLockFixture();
+        
+        await ethers.provider.send("evm_setNextBlockTimestamp", [unlockTime]);
+        await ethers.provider.send("evm_mine", []);
+
+        await expect(lock.withdraw())
+          .to.emit(lock, "Withdrawal")
+          .withArgs(lockedAmount, unlockTime);
+      });
+    });
+
+    describe("Transfers", function () {
+      it("Should transfer the funds to the owner", async function () {
+        const { lock, unlockTime, lockedAmount, owner } = await deployOneYearLockFixture();
+        
+        await ethers.provider.send("evm_setNextBlockTimestamp", [unlockTime]);
+        await ethers.provider.send("evm_mine", []);
+
+        const ownerBalanceBefore = await ethers.provider.getBalance(owner.address);
+        await lock.withdraw();
+        const ownerBalanceAfter = await ethers.provider.getBalance(owner.address);
+        
+        expect(ownerBalanceAfter.sub(ownerBalanceBefore)).to.equal(lockedAmount);
+      });
+    });
+  });
+});
