1fc76dab72f10f753fdd0687a492fd7f1e2bd905
Ayush765-spec <ayushsouthpoint@gmail.com>
2025-12-30 12:30:30 +0530

    add assests and components made and integrated in App.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import {HIMenuAlt4} from 'react-icons/hi'
+import{AiOUtlineClose} from 'react-icons/ai'
+
+import logo from '../../images/logo.png'
+
+const Navbar=()=>{
+    return(
+        <nav className="w-full flex md:justify-between">
+
+
+        </nav>
+
+    )
+    
+
+}
+export default Navbar
---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+export {deafult as Loader} from './Loader'
+export {deafult as Navbar} from './Navbar'
+export {deafult as Footer} from './Footer'
+export {deafult as Welcome} from './Welcome'
+export {deafult as Transactions} from './Transactions'
+export {deafult as Services} from './Services'
+
